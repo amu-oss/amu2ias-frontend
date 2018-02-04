d0c710f868485892999c85f674fd958f2b81cd2a
Areeb Jamal <jamal.areeb@gmail.com>
2018-02-05 05:17:01 +0530

    chore: Add surge deployment (#5)

---
+++ .circleci/deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+export DEPLOY_BRANCH=${DEPLOY_BRANCH:-master}
+
+if [ ! -z "$CIRCLE_PR_USERNAME" -o "$CIRCLE_PROJECT_USERNAME" != "amu-oss" -o "$CIRCLE_BRANCH" != "$DEPLOY_BRANCH" ]; then
+	echo "This is $CIRCLE_PR_USERNAME building the PR $CIRCLE_PULL_REQUEST triggered by $CIRCLE_USERNAME"
+    echo "We deploy site only for changes in master. So, let's skip this shall we ? :)"
+    exit 0
+fi
+
+yarn global add surge
+
+export SURGE_LOGIN=amu-oss@gmail.com
+surge --project ./dist --domain amu2ias.surge.sh
+
