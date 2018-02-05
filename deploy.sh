5bb38eb74f1af51c39d84480a5e092bcd11430fb
Areeb Jamal <jamal.areeb@gmail.com>
2018-02-05 05:45:19 +0530

    chore: Repair surge (#6)
    chore: Repair surge

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 export DEPLOY_BRANCH=${DEPLOY_BRANCH:-master}
 
 if [ ! -z "$CIRCLE_PR_USERNAME" -o "$CIRCLE_PROJECT_USERNAME" != "amu-oss" -o "$CIRCLE_BRANCH" != "$DEPLOY_BRANCH" ]; then
-	echo "This is $CIRCLE_PR_USERNAME building the PR $CIRCLE_PULL_REQUEST triggered by $CIRCLE_USERNAME"
+    echo "This is $CIRCLE_PR_USERNAME building the PR $CIRCLE_PULL_REQUEST triggered by $CIRCLE_USERNAME"
     echo "We deploy site only for changes in master. So, let's skip this shall we ? :)"
     exit 0
 fi
