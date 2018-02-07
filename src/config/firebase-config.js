a94fccca075b27b5cde54b75d078c3fa2b32a0e6
Mohd Omama <mohd.omama@gmail.com>
2018-02-07 21:41:39 +0530

    Add firebase config

---
+++ src/config/firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyCqWTfvsBmk52-spE-x3OBaPmaubsgd-tI',
+  authDomain: 'amu2ias-firebase.firebaseapp.com',
+  databaseURL: 'https://amu2ias-firebase.firebaseio.com',
+  projectId: 'amu2ias-firebase',
+  storageBucket: 'amu2ias-firebase.appspot.com',
+  messagingSenderId: '829286748577'
+}
