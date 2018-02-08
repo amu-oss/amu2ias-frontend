36e604bb1d1d0bf84b4be9b5eee2a7460fde9a91
Mohd Omama <mohd.omama@gmail.com>
2018-02-08 22:37:23 +0530

    Change color of signin card. Correct comments in store/index.js

--- src/components/Signin.vue
+++ src/components/Signin.vue
@@ -2,7 +2,7 @@
   <v-container>
     <v-layout row v-if="user">
       <v-flex xs12 sm8 lg6 xl4 offset-sm2 offset-lg3 offset-xl4 class="text-xs-center">
-        <v-card>
+        <v-card color="indigo" dark>>
             <div class="display-1 text-xs-center pt-3">You are already logged in!</div>
             <v-btn error large @click="logout" class="ma-3">Logout</v-btn>
           </v-card>
@@ -10,7 +10,7 @@
     </v-layout>
     <v-layout row v-show="!user">
       <v-flex xs12 sm8 lg6 xl4 offset-sm2 offset-lg3 offset-xl4>
-        <v-card>
+        <v-card color="blue lighten-2" dark>
             <div class="display-1 text-xs-center pt-3">Sign In or Register</div>
             <div id="firebaseui-auth-container" class="pt-3 pb-3"></div>
           </v-card>
