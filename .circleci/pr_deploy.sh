d49c7ee10a77da2ea877b1b7b9f199c9b72a2352
Areeb Jamal <jamal.areeb@gmail.com>
2018-02-09 05:57:29 +0530

    chore: Add lockfile updater

--- .circleci/pr_deploy.sh
+++ .circleci/pr_deploy.sh
@@ -1,15 +1,24 @@
 #!/usr/bin/env bash
 
-if [ -z "$CIRCLE_PR_USERNAME" -o "$CIRCLE_PROJECT_USERNAME" != "amu-oss" ]; then
+if [ -z "$CIRCLE_PULL_REQUEST" -o "$CIRCLE_PROJECT_USERNAME" != "amu-oss" ]; then
     echo "Not a PR. Skipping surge deployment"
     exit 0
 fi
 
-yarn add surge
-
 export SURGE_LOGIN=amu-oss@gmail.com
 
-export DEPLOY_DOMAIN=https://pr-${CIRCLE_PR_NUMBER}-${CIRCLE_PR_USERNAME}-${CIRCLE_PR_REPONAME}.surge.sh
-./node_modules/.bin/surge --project ./dist --domain $DEPLOY_DOMAIN
+if [ -z "${CIRCLE_PR_NUMBER}" ]; then
+    export ISSUE_NUMBER=$(echo ${CIRCLE_PULL_REQUEST} | awk -F '/' ' { print $(NF) } ')
+    export CONTEXT=${CIRCLE_BRANCH}
+    export REPONAME=${CIRCLE_PROJECT_REPONAME}
+else
+    export ISSUE_NUMBER=${CIRCLE_PR_NUMBER}
+    export CONTEXT=${CIRCLE_PR_USERNAME}
+    export REPONAME=${CIRCLE_PR_REPONAME}
+fi
 
-curl --request POST "https://api.github.com/repos/amu-oss/amu2ias-frontend/issues/${CIRCLE_PR_NUMBER}/comments?access_token=${GITHUB_API_TOKEN}" --data '{"body":"PR Successfully Deployed : '"${DEPLOY_DOMAIN}"'"}'
+export DEPLOY_DOMAIN=https://pr-${ISSUE_NUMBER}-${CONTEXT}-${REPONAME}.surge.sh
+
+surge --project ./dist --domain $DEPLOY_DOMAIN
+
+curl --request POST "https://api.github.com/repos/amu-oss/amu2ias-frontend/issues/${ISSUE_NUMBER}/comments?access_token=${GH_TOKEN}" --data '{"body":"PR Successfully Deployed : '"${DEPLOY_DOMAIN}"'"}'
