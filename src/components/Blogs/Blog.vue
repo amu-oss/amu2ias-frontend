833799f0705d116416972773e295b9a5a2cb52d6
Mohd Omama <mohd.omama@gmail.com>
2018-02-11 14:04:13 +0530

    Push the blog layout into a component

--- src/components/Blogs/Blog.vue
+++ src/components/Blogs/Blog.vue
@@ -1,32 +1,28 @@
 <template>
-  <v-container grid-list-lg>
-  <v-layout row wrap>
-    <v-flex xs12 sm10 md8 offset-sm1 offset-md2>
-      <v-card>
-        <v-card-title primary-title style="backgroundColor: LightSlateGray" >
-          <div>
-            <div class="headline">{{ title }}</div>
-          </div>
-        </v-card-title>
-        <v-card-text>
-        <div>
-        <div class="grey--text">Category: {{ category }}</div>
-        <div class="grey--text">Author: {{ author }}</div>
-        <div class="grey--text">Tags: {{ tags }}</div>
-        </div>
-        </v-card-text>
-        <v-card-actions>
-          <v-btn icon @click.native="show = !show">
-            <v-icon>{{ show ? 'keyboard_arrow_up' : 'keyboard_arrow_down' }}</v-icon>
-          </v-btn>
-        </v-card-actions>
-          <v-card-text v-show="show">
-            {{ description  }}
-          </v-card-text>
-      </v-card>
-    </v-flex>
-  </v-layout>
-  </v-container>
+<v-flex xs12 sm10 md8 offset-sm1 offset-md2>
+  <v-card>
+    <v-card-title primary-title style="backgroundColor: LightSlateGray" >
+      <div>
+        <div class="headline">{{ title }}</div>
+      </div>
+    </v-card-title>
+    <v-card-text>
+    <div>
+    <div class="grey--text">Category: {{ category }}</div>
+    <div class="grey--text">Author: {{ author }}</div>
+    <div class="grey--text">Tags: {{ tags }}</div>
+    </div>
+    </v-card-text>
+    <v-card-actions>
+      <v-btn icon @click.native="show = !show">
+        <v-icon>{{ show ? 'keyboard_arrow_up' : 'keyboard_arrow_down' }}</v-icon>
+      </v-btn>
+    </v-card-actions>
+      <v-card-text v-show="show">
+        {{ description  }}
+      </v-card-text>
+  </v-card>
+</v-flex>
 </template>
 
 <script>
