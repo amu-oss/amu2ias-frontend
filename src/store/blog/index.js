8bdf4640e249c5b594b6be0ea21fab33fca72281
Areeb Jamal <jamal.areeb@gmail.com>
2018-02-13 10:15:22 +0530

    feat: Add pagination and improve layout

--- src/store/blog/index.js
+++ src/store/blog/index.js
@@ -2,35 +2,68 @@ import firebase from '@/firebase'
 import { firebaseAction } from 'vuexfire'
 
 const firestore = firebase.firestore()
-const getBlogRef = () => firestore.collection('blogs')
+const blogsRef = firestore.collection('blogs').orderBy('created', 'desc')
+
+const getLast = array => array[array.length - 1]
+
 export default{
   namespaced: true,
   state: {
-    // blog related state
     blogs: [],
-    blogsRef: null
+    pageStack: [],
+    lastRef: null,
+    timeout: false
   },
 
   getters: {
-    // blog related getters
-    blogs: state => state.blogs.slice().reverse()
+    empty: state => state.blogs.length <= 0,
+    prev: state => state.pageStack.length > 0, // Tells if you can go back
+    next: (state, getters) => !getters.empty, // Tells if you can go forward
+    prevPage: (state, getters) => getters.prev ? getLast(state.pageStack) : null,
+    nextPage: (state, getters) => getters.next ? getLast(state.blogs) : null,
+    // If timeout occurs and still no blogs are present, it is loading failure
+    loadingFailure: (state, getters) => getters.empty && state.timeout
   },
 
   actions: {
-    // blog related actions
-    fetchBlogs: ({ commit, dispatch }) => {
-      commit('setBlogRef')
-      dispatch('fetchBlogsData', getBlogRef())
-    },
-    fetchBlogsData: firebaseAction(({ bindFirebaseRef }, blogsRef) => {
+    bindBlogs: firebaseAction(({ state, commit, bindFirebaseRef }, blogsRef) => {
+      // Do not refetch same data again and again
+      if (blogsRef === state.lastRef) {
+        return
+      }
+
+      // Update the last reference to current one
+      commit('setLastRef', blogsRef)
+      // Reset timeout
+      commit('setTimeout', false)
       bindFirebaseRef('blogs', blogsRef)
-    })
+      setTimeout(() => {
+        commit('setTimeout', true)
+      }, 3000)
+    }),
+
+    fetchBlogs: ({ dispatch }, ref) => {
+      dispatch('bindBlogs', (ref || blogsRef).limit(5))
+    },
+
+    fetchNext: ({ commit, getters, dispatch }) => {
+      commit('pushPage')
+      dispatch('fetchBlogs', blogsRef.startAfter(getters.nextPage.created))
+    },
+
+    fetchPrevious: ({ commit, getters, dispatch }) => {
+      dispatch('fetchBlogs', getters.prevPage)
+      commit('popPage')
+    }
   },
 
   mutations: {
-    // blog related mutations
-    setBlogRef: (state) => {
-      state.blogsRef = getBlogRef()
-    }
+    setLastRef: (state, lastRef) => (state.lastRef = lastRef),
+
+    setTimeout: (state, timeout) => (state.timeout = timeout),
+
+    pushPage: (state) => state.pageStack.push(state.lastRef),
+
+    popPage: (state) => state.pageStack.pop()
   }
 }
