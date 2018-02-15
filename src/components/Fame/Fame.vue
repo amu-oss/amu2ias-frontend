438a9ad30cac82f7fccf7b822b8b85f31f0dae51
Mohd Omama <mohd.omama@gmail.com>
2018-02-15 19:56:53 +0530

    Add basic layout

---
+++ src/components/Fame/Fame.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-container>
+    <v-flex xs12 sm10 md8 offset-sm1 offset-md2>
+      <v-container grid-list-md>
+        <v-layout row wrap  justify-space-around>
+          <v-flex d-flex xs12 sm6 md6 lg4 v-for="developer in 4" :key="developer.id">
+            <FameCard></FameCard>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-flex>
+  </v-container>
+</template>
+
+<script>
+import FameCard from './FameCard'
+
+export default {
+  components: {
+    FameCard
+  },
+  data () {
+    return {
+      logo_url: this.$urls.staticUrls.blog
+    }
+  },
+  head: {
+    title () {
+      return {
+        inner: this.$route.name
+      }
+    }
+  }
+}
+</script>
---
+++ src/components/Fame/FameCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-card raised class="text-xs-center" color='amber'>
+      <v-flex d-flex xs12>
+      <v-avatar size="200px" class="mt-2">
+        <img src="https://lh4.googleusercontent.com/-WPHLodvF-hc/AAAAAAAAAAI/AAAAAAAAE3A/945czOQqTbE/photo.jpg" alt="alt name">
+      </v-avatar>
+      </v-flex>
+      <v-flex d-flex xs12>
+         <v-card-text class="headline"> Name of the dude </v-card-text><br>
+      </v-flex>
+      <v-flex>
+        <div class="title" color="grey--text">AIR: 217</div><br>
+        <div class="subheading">CSE 2017 (IPS)</div><br>
+        <div class="subheading">Some Links for the dude</div><br>
+      </v-flex>
+  </v-card>
+</template>
